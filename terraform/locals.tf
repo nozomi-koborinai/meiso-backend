b29493d034b8958d3190ff9948e01d0c434b8e34
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-05-11 17:18:18 +0900

    feat: setup firestore

---
+++ terraform/locals.tf
@@ -0,0 +1,41 @@
+locals {
+  # Firestore master data
+  docs = [
+    {
+      collection  = "quiz"
+      document_id = "question-1"
+      fields = jsonencode({
+        "question" = {
+          "stringValue" = "First Question"
+        },
+        "answer" = {
+          "stringValue" = "Answer 1"
+        },
+      })
+    },
+    {
+      collection  = "quiz"
+      document_id = "question-2"
+      fields = jsonencode({
+        "question" = {
+          "stringValue" = "Second Question"
+        },
+        "answer" = {
+          "stringValue" = "Answer 2"
+        },
+      })
+    },
+    {
+      collection  = "quiz"
+      document_id = "question-3"
+      fields = jsonencode({
+        "question" = {
+          "stringValue" = "Third Question"
+        },
+        "answer" = {
+          "stringValue" = "Answer 3"
+        },
+      })
+    },
+  ]
+}
